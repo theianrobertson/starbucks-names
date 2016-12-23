99390871fc0e3fe7e12a6064050455226f1edd9f
Ian Robertson <ianrobertson85@gmail.com>
2016-12-22 21:45:39 -0500

    Adding in cup data

--- config.py
+++ config.py
@@ -7,19 +7,77 @@ OPTIONS = {
     'bias': 0.3,
     'samples': 1
 }
+CUPS_DIR = 'cups'
 NAMES_DIR = 'names'
 
-'300px-Starbucks_11.jpg'
-'54f94bc6024c9_-_starbucks-cup-del1212.jpg'
-'A-cup-displaying-the-Starbucks-Coffee-logo.jpg'
-'AR-151119871.jpg'
-'Starbucks-Cup.jpg'
-'cup.png'
-'cup2.png'
-'cup3.png'
-'enhanced-15790-1447088848-3.png'
-'image.jpg'
-'new-starbucks-cup-design-front.jpg'
-'other'
-'starbucks-red-cup.jpg'
-'starbucksholidaycup-1024x1024.jpg'
+CUPS = [
+    {
+        'filename': 'cup1.jpg',
+        'x_top': 190,
+        'y_top': 200,
+        'x_bottom': 474,
+        'y_bottom': 292
+    },
+    {
+        'filename': 'cup2.jpg',
+        'x_top': 300,
+        'y_top': 180,
+        'x_bottom': 500,
+        'y_bottom': 260
+    },
+    {
+        'filename': 'cup3.jpg',
+        'x_top': 300,
+        'y_top': 175,
+        'x_bottom': 500,
+        'y_bottom': 230
+    },
+    {
+        'filename': 'cup4.jpg',
+        'x_top': 180,
+        'y_top': 180,
+        'x_bottom': 440,
+        'y_bottom': 292
+    },{
+        'filename': 'cup5.jpg',
+        'x_top': 175,
+        'y_top': 175,
+        'x_bottom': 420,
+        'y_bottom': 300
+    },
+    {
+        'filename': 'cup6.jpg',
+        'x_top': 370,
+        'y_top': 81,
+        'x_bottom': 505,
+        'y_bottom': 147
+    },
+    {
+        'filename': 'cup7.jpg',
+        'x_top': 75,
+        'y_top': 89,
+        'x_bottom': 182,
+        'y_bottom': 117
+    },
+    {
+        'filename': 'cup8.jpg',
+        'x_top': 310,
+        'y_top': 205,
+        'x_bottom': 545,
+        'y_bottom': 385
+    },
+    {
+        'filename': 'cup9.jpg',
+        'x_top': 95,
+        'y_top': 69,
+        'x_bottom': 250,
+        'y_bottom': 145
+    },
+    {
+        'filename': 'cup10.jpg',
+        'x_top': 130,
+        'y_top': 125,
+        'x_bottom': 350,
+        'y_bottom': 240
+    }
+]
